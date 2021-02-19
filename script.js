a1d978398fdd711c50fb8b6e81a8c2cba3b400e0
nickbulm <nicholas.bulmer93@gmail.com>
2021-02-19 21:23:56 +0000

    done

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ function compute()
     let years = Number(document.getElementById("years").value);
     let interest = principal * years * rate / 100
     let year = Number(new Date().getFullYear()) + years
-
-    document.getElementById("result").innerHTML =   `If you deposit: <span class="number">$${principal}</span><br>
+    if(principal <= 0 || isNaN(principal) || principal === undefined) {
+        document.getElementById("compute").disabled = true
+        document.getElementById("amountval").innerText = "Must be a positive number"
+        document.getElementById("result").innerHTML = null
+    } else {
+        document.getElementById("compute").disabled = false
+        document.getElementById("amountval").innerText = null
+        document.getElementById("result").innerHTML =   `If you deposit: <span class="number">$${principal}</span><br>
                                                     at an interest rate of: <span class="number">${rate}</span><br>
                                                     You will receive an amount of: <span class="number">$${interest}</span><br>
                                                     in the year: <span class="number">${year}</span>`
+    }
 }
 function validate(e) {
     let principal = e.target.value
-    if(principal <= 0 || isNaN(principal)) {
+    if(principal <= 0 || isNaN(principal) || principal === undefined) {
         document.getElementById("compute").disabled = true
         document.getElementById("amountval").innerText = "Must be a positive number"
     } else {
